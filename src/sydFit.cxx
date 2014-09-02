ad43257aad31ab43155947e42392a625cccfc221
David Sarrut <david.sarrut@creatis.insa-lyon.fr>
2014-09-02 13:49:49 +0200

    Restructure, split sydQuery

---
+++ src/sydFit.cxx
@@ -0,0 +1,158 @@
+/*=========================================================================
+  Program:   syd
+
+  Authors belong to:
+  - University of LYON              http://www.universite-lyon.fr/
+  - Léon Bérard cancer center       http://www.centreleonberard.fr
+  - CREATIS CNRS laboratory         http://www.creatis.insa-lyon.fr
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the copyright notices for more information.
+
+  It is distributed under dual licence
+
+  - BSD        See included LICENSE.txt file
+  - CeCILL-B   http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html
+  ===========================================================================**/
+
+#include "sydFit.h"
+
+// --------------------------------------------------------------------
+struct syd::MonoExponentialResidual {
+  MonoExponentialResidual(double x, double y, double w): x_(x), y_(y), w_(w) {}
+  template <typename T> bool operator()(const T* const A,
+                                        const T* const lambda,
+                                        T* residual) const {
+    residual[0] = w_ * (T(y_) - A[0]*exp(-lambda[0] * T(x_)));
+    return true;
+
+  }
+private:
+  const double x_;
+  const double y_;
+  const double w_;
+};
+// --------------------------------------------------------------------
+
+
+// --------------------------------------------------------------------
+struct syd::BiExponentialResidual {
+  BiExponentialResidual(double x, double y): x_(x), y_(y) {}
+  template <typename T> bool operator()(const T* const A,
+                                        const T* const lambda,
+                                        const T* const B,
+                                        const T* const lambda2,
+                                        T* residual) const {
+    //    residual[0] = T(y_) - (A[0]*exp(-lambda[0] * T(x_)) + B[0]*exp(-lambda2[0] * T(x_)));
+    residual[0] = T(y_) - A[0]*(B[0]*exp(-lambda[0] * T(x_)) + (1.0-B[0])*exp(-lambda2[0] * T(x_)));
+    return true;
+
+  }
+private:
+  const double x_;
+  const double y_;
+};
+// --------------------------------------------------------------------
+
+
+// --------------------------------------------------------------------
+struct syd::BiExponentialResidual2 {
+  BiExponentialResidual2(double x, double y): x_(x), y_(y) {}
+  template <typename T> bool operator()(const T* const A,
+                                        const T* const lambda,
+                                        const T* const lambda2,
+                                        T* residual) const {
+    residual[0] = T(y_) -
+      ((lambda[0]*lambda2[0])/(A[0]*(lambda2[0]-lambda[0]))) *
+      (exp(-lambda[0]*T(x_)) - exp(-lambda2[0]*T(x_)));
+    return true;
+
+  }
+private:
+  const double x_;
+  const double y_;
+};
+// --------------------------------------------------------------------
+
+
+// --------------------------------------------------------------------
+void syd::Fit(std::vector<double> & times,
+              std::vector<double> & activities,
+              std::vector<double> & variances) {
+
+  // Param initialisation
+  double A = activities[1]; // not zero
+  double lambda = Lambda_Indium;
+  double B = 0.5;//activities[0];
+  double lambda2 = Lambda_Indium/2.0;
+  DD(A);
+  DD(lambda);
+  DD(B);
+  DD(lambda2);
+
+  int kNumObservations = times.size();
+  DD(kNumObservations);
+
+  // // Weight according to variance
+  // double w = 0.0;
+  // for(auto i=0; i<kNumObservations; i++) {
+  //   activities[i] = activities[i]/variances[i];
+  //   w += 1.0/variances[i];
+  // }
+  // DD(w);
+
+  // typedef to short the code
+  // 1,1,1 ==> dimension of residual, dimension of parameters
+  typedef ceres::AutoDiffCostFunction<MonoExponentialResidual, 1, 1, 1> CostFctType2;
+  typedef ceres::AutoDiffCostFunction<BiExponentialResidual2, 1, 1, 1, 1> CostFctType3;
+  typedef ceres::AutoDiffCostFunction<BiExponentialResidual, 1, 1, 1, 1, 1> CostFctType4;
+
+  // declare obj function
+  ceres::Problem problem;
+  for (int i = 0; i < kNumObservations; ++i) {
+    problem.AddResidualBlock(new CostFctType2(new MonoExponentialResidual(times[i], activities[i], variances[i])),
+      NULL,
+      &A, &lambda);
+    /*problem.AddResidualBlock(new CostFctType4(new BiExponentialResidual(times[i], activities[i])),
+      NULL,
+      &A, &lambda, &B, &lambda2);
+    */
+    /*problem.AddResidualBlock(new CostFctType3(new BiExponentialResidual2(times[i], activities[i])),
+                             NULL,
+                             &A, &lambda, &lambda2);
+    */
+  }
+
+  // Bounds (constraints)
+  problem.SetParameterLowerBound(&A, 0, 0); // A positive
+  // problem.SetParameterLowerBound(&B, 0, 0); // B positive
+  // //  problem.SetParameterUpperBound(&B, 0, 1); // B <=1.0
+  problem.SetParameterLowerBound(&lambda, 0, 0); // positive
+  // problem.SetParameterLowerBound(&lambda2, 0, 0); // positive
+  // problem.SetParameterLowerBound(&lambda, 0, Lambda_Indium*0.9);
+  // problem.SetParameterUpperBound(&lambda, 0, Lambda_Indium*1.1);
+
+  // Solve
+  ceres::Solver::Options options;
+  options.max_num_iterations = 100;
+  options.linear_solver_type = ceres::DENSE_QR;
+  options.minimizer_progress_to_stdout = false;
+  options.trust_region_strategy_type = ceres::LEVENBERG_MARQUARDT; // LM is the default
+  // may try Dogleg also
+
+  // output
+  ceres::Solver::Summary summary;
+  Solve(options, &problem, &summary);
+  std::cout << summary.BriefReport() << "\n";
+
+  // for gp
+  DD(Lambda_Indium);
+  std::cout << "A = " << A << std::endl
+            << "lambda = " << lambda << std::endl
+            // << "B = " << B << std::endl
+            // << "lambda2 = " << lambda2 << std::endl
+    ;
+
+}
+// --------------------------------------------------------------------
---
+++ src/sydFit.h
@@ -0,0 +1,45 @@
+/*=========================================================================
+  Program:   syd
+
+  Authors belong to:
+  - University of LYON              http://www.universite-lyon.fr/
+  - Léon Bérard cancer center       http://www.centreleonberard.fr
+  - CREATIS CNRS laboratory         http://www.creatis.insa-lyon.fr
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the copyright notices for more information.
+
+  It is distributed under dual licence
+
+  - BSD        See included LICENSE.txt file
+  - CeCILL-B   http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html
+  ===========================================================================**/
+
+#ifndef SYDFIT_H
+#define SYDFIT_H
+
+// Ceres
+#include <ceres/ceres.h>
+#include <glog/logging.h>
+
+// syd
+#include "sydTimeActivityCurve.h"
+#include "sydQuery.h"
+
+#include "clitkCommon.h"
+
+// --------------------------------------------------------------------
+namespace syd {
+
+  struct MonoExponentialResidual;
+  struct BiExponentialResidual;
+  struct BiExponentialResidual2;
+
+  void Fit(std::vector<double> & times,
+           std::vector<double> & activities,
+           std::vector<double> & variances);
+
+}
+
+#endif
