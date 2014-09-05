f2f153e7577054d7d3e81546569339043aa7d6f3
David Sarrut <david.sarrut@creatis.insa-lyon.fr>
2014-09-05 16:50:25 +0200

    New syd version (empty)

---
+++ src/syd.cxx
@@ -0,0 +1,32 @@
+/*=========================================================================
+  Program:   syd
+
+  Authors belong to:
+  - University of LYON              http://www.universite-lyon.fr/
+  - Léon Bérard cancer center       http://www.centreleonberard.fr
+  - CREATIS CNRS laboratory         http://www.creatis.insa-lyon.fr
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the copyright notices for more information.
+
+  It is distributed under dual licence
+
+  - BSD        See included LICENSE.txt file
+  - CeCILL-B   http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html
+  ===========================================================================**/
+
+// clitk
+#include "clitkCommon.h"
+
+// syd
+#include "syd_ggo.h"
+
+// --------------------------------------------------------------------
+int main(int argc, char* argv[])
+{
+  // Init command line
+  GGO(syd, args_info);
+
+}
+// --------------------------------------------------------------------
