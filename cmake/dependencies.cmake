b22ceb48c2013ceaf76bc03a130383b8c36550b7
David Sarrut <david.sarrut@creatis.insa-lyon.fr>
2015-05-14 18:22:31 +0200

    Clean makefile

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -1,24 +1,11 @@
 
 #----------------------------------------------------------
 # Find ITK (required)
-find_package(ITK)
-if(ITK_FOUND)
-  include("${ITK_USE_FILE}")
-else(ITK_FOUND)
+find_package(ITK REQUIRED)
+if(NOT ITK_FOUND)
   message(FATAL_ERROR "Cannot build without ITK.  Please set ITK_DIR.")
-endif(ITK_FOUND)
-#----------------------------------------------------------
-
-
-#----------------------------------------------------------
-# this is required (even if gdcm is inside itk)
-# *********** FIXME : only needed on satyre ?
-
-if(ITK_USE_SYSTEM_GDCM)
-  message(STATUS "use gdcm")
-  find_package(GDCM REQUIRED)
-  include(${GDCM_USE_FILE})
-endif()
+endif(NOT ITK_FOUND)
+include(${ITK_USE_FILE})
 #----------------------------------------------------------
 
 
@@ -26,4 +13,11 @@ endif()
 # Find gengetopt, will create a target exe if not found
 set(CMAKE_MODULE_PATH "${SYD_SOURCE_DIR}/cmake" ${CMAKE_MODULE_PATH})
 find_package(Gengetopt)
+#----------------------------------------------------------
+
+
+#----------------------------------------------------------
+# Find dcmtk
+find_package(DCMTK REQUIRED)
+include_directories(${DCMTK_INCLUDE_DIRS})
 #----------------------------------------------------------
