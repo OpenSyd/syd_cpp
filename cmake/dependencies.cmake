07675e0a79261b5b84d4d00e38f36b51531b6277
David Sarrut <david.sarrut@creatis.insa-lyon.fr>
2015-06-09 10:10:08 +0200

    Add compile c++11 (and odb cmake)

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -4,3 +4,10 @@
 find_package(DCMTK REQUIRED)
 include_directories(${DCMTK_INCLUDE_DIRS})
 #----------------------------------------------------------
+
+
+#----------------------------------------------------------
+# Find Odb
+find_package(ODB REQUIRED OPTIONAL_COMPONENTS sqlite)
+include(${ODB_USE_FILE})
+#----------------------------------------------------------
