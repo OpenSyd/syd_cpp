54d41fe1037e090a7427f72351d4e9f2540a2a25
David Sarrut <david.sarrut@creatis.insa-lyon.fr>
2015-10-09 16:29:41 +0200

    remove check here. Remove several steps

--- src/std_db/sydInsertIntegratedActivityImage.cxx
+++ src/std_db/sydInsertIntegratedActivityImage.cxx
@@ -63,25 +63,26 @@ int main(int argc, char* argv[])
     return EXIT_SUCCESS;
   }
 
-  // Make some check on images list
-  auto size = images[0]->size;
+  // Check same injection date
+  images[0]->FatalIfNoDicom();
+  syd::Injection::pointer injection = images[0]->dicoms[0]->injection;
   bool b = true;
   for(auto image:images) {
-    b = b and image->IsSameSizeAndSpacingThan(images[0]);
+    image->FatalIfNoDicom();
+    b = b and (injection->id == image->dicoms[0]->injection->id);
   }
   if (!b) {
-    std::string s;
-    for(auto image:images) s += image->SizeAsString() + " " + image->SpacingAsString() + "\n";
-    LOG(FATAL) << "The images must have the same size/spacing, abort. Images are: " << s;
+    LOG(FATAL) << "The image do not have the same injection.";
   }
 
-  // Sort by time
-  std::sort(begin(images), end(images),
-            [images](syd::Image::pointer a, syd::Image::pointer b) {
-              if (a->dicoms.size() == 0) return true;
-              if (b->dicoms.size() == 0) return false;
-              return a->dicoms[0]->acquisition_date < b->dicoms[0]->acquisition_date;
-            });
+  // Check same pixel units (warning)
+  for(auto image:images) {
+    if (image->pixel_value_unit->name != "MBq_by_IA") {
+      LOG(WARNING) << "I expected pixel value unit to be 'MBq_by_IA', while it is "
+                   << image->pixel_value_unit->name << "for the image:"
+                   << std::endl << image;
+    }
+  }
 
   // image type
   typedef float PixelType;
@@ -89,26 +90,19 @@ int main(int argc, char* argv[])
   typedef itk::ImageRegionIterator<ImageType> Iterator;
   typedef itk::NeighborhoodIterator<ImageType> NIterator;
 
-  // Create main builder
-  syd::IntegratedActivityImageBuilder builder;
-
   // Read the images+times and set to the builder
-  std::string starting_date = images[0]->dicoms[0]->injection->date;
+  std::string starting_date = injection->date;
   ImageType::Pointer im;
   std::vector<ImageType::Pointer> itk_images;
+  std::vector<double> times;
   for(auto image:images) {
     im = syd::ReadImage<ImageType>(db->GetAbsolutePath(image));
     if (args_info.gauss_arg != 0) im = syd::GaussianFilter<ImageType>(im, args_info.gauss_arg);
     double t = syd::DateDifferenceInHours(image->dicoms[0]->acquisition_date, starting_date);
-    builder.AddInput(im, t);
+    times.push_back(t);
     itk_images.push_back(im);
   }
   im = itk_images[0]; // consider the first image for the following
-
-  // Set some options
-  builder.image_lambda_phys_in_hour_ = log(2.0)/images[0]->dicoms[0]->injection->radionuclide->half_life_in_hours;
-  builder.debug_only_flag_ = args_info.debug_only_flag;
-  builder.R2_min_threshold_ = args_info.r2_min_arg;
 
   // Consider the debug points
   struct debug_point {
@@ -125,43 +119,21 @@ int main(int argc, char* argv[])
       if (is and d.name[0] != '#') debug_points.push_back(d);
     }
   }
-  for(auto d:debug_points) builder.AddDebugPixel(d.name, d.x, d.y, d.z);
 
-  // Set the models
-  auto f2 = new syd::FitModel_f2;
-  f2->id_ = 2;
-  auto f3 = new syd::FitModel_f3;
-  f3->id_ = 3;
+  // Create some models
+  auto f2  = new syd::FitModel_f2;
+  auto f3  = new syd::FitModel_f3;
   auto f4a = new syd::FitModel_f4a;
-  f4a->id_ = 4;
-  //builder.AddModel(f2); // no f2
-  builder.AddModel(f3);
-  builder.AddModel(f4a);
+  auto f4  = new syd::FitModel_f4;
 
-  // auto f4 = new syd::FitModel_f4;
-  // f4->id_ = 7;
-  // builder.AddModel(f4); // no f4
-
-  // Set the output
-  auto auc = new syd::FitOutputImage_AUC(im);
-  auc->l_phys = builder.image_lambda_phys_in_hour_;
-  auto auc_177lu = new syd::FitOutputImage_AUC(im);
+  // Create some output types
+  auto auc = new syd::FitOutputImage_AUC(im, injection->GetLambdaInHours());
   auto r2 = new syd::FitOutputImage_R2(im);
   auto best_model = new syd::FitOutputImage_Model(im);
   auto iter = new syd::FitOutputImage_Iteration(im);
   auto success = new syd::FitOutputImage_Success(im);
-  builder.AddOutputImage(auc);
-  builder.AddOutputImage(success); // needed for mask
-  if (args_info.debug_images_flag) {
-    builder.AddOutputImage(r2);
-    builder.AddOutputImage(best_model);
-    builder.AddOutputImage(iter);
-    auc_177lu->l_phys = 0.0043449876; // lut
-    //    auc_177lu->l_phys = 0.010297405; // Indium
-    //    auc_177lu->l_phys = 0.010823439;
-    auc_177lu->filename = "auc_177lu.mhd";
-    builder.AddOutputImage(auc_177lu);
-  }
+  auto eff_half_life = new syd::FitOutputImage_EffHalfLife(im);
+  auto nb_points = new syd::FitOutputImage_NbOfPointsForFit(im);
 
   // Use a mask, consider values of the first spect
   ImageType::Pointer mask = syd::CreateImageLike<ImageType>(im);
@@ -175,14 +147,41 @@ int main(int argc, char* argv[])
     ++it_mask;
     ++it_image;
   }
-  builder.SetMask(mask);
   if (args_info.debug_images_flag)
     syd::WriteImage<ImageType>(mask, "mask.mhd");
+
+
+  // Create main builder
+  syd::IntegratedActivityImageBuilder builder;
+  for(auto i=0; i<times.size(); i++) builder.AddInput(itk_images[i], times[i]);
+
+  // FIXME
+  builder.image_lambda_phys_in_hour_ = injection->GetLambdaInHours();
+  builder.debug_only_flag_ = args_info.debug_only_flag;
+  builder.R2_min_threshold_ = args_info.r2_min_arg;
+  builder.restricted_tac_flag_ = true; // FIXME
+
+  for(auto d:debug_points) builder.AddDebugPixel(d.name, d.x, d.y, d.z);
+  builder.AddOutputImage(auc);
+  if (args_info.debug_images_flag) {
+    builder.AddOutputImage(success);
+    builder.AddOutputImage(r2);
+    builder.AddOutputImage(best_model);
+    builder.AddOutputImage(iter);
+    builder.AddOutputImage(eff_half_life);
+    builder.AddOutputImage(nb_points);
+  }
+  builder.SetMask(mask);
+
+  builder.AddModel(f2, 2); // no f2 ?
+  builder.AddModel(f3, 3);
+  builder.AddModel(f4a, 4);
+  builder.AddModel(f4, 5); // no f4 ?
 
   // Go !
   builder.CreateIntegratedActivityImage();
 
-  // Output
+  // Output FIXME
   if (args_info.debug_images_flag and !args_info.debug_only_flag)
     for(auto o:builder.outputs_) syd::WriteImage<ImageType>(o->image, o->filename);
 
@@ -190,57 +189,57 @@ int main(int argc, char* argv[])
   builder.SaveDebugPixel("gp/tac.txt");
   builder.SaveDebugModel("gp/models.txt");
 
-  // Modifiy the mask according to success
-  auto it_success = success->iterator;
-  it_success.GoToBegin();
-  it_mask.GoToBegin();
-  while (!it_mask.IsAtEnd()) {
-    if (it_success.Get() == 1.0) it_mask.Set(0.0);
-    ++it_success;
-    ++it_mask;
-  }
-  if (args_info.debug_images_flag and !args_info.debug_only_flag)
-    syd::WriteImage<ImageType>(mask, "mask2.mhd");
+  // // Modifiy the mask according to success
+  // auto it_success = success->iterator;
+  // it_success.GoToBegin();
+  // it_mask.GoToBegin();
+  // while (!it_mask.IsAtEnd()) {
+  //   if (it_success.Get() == 1.0) it_mask.Set(0.0);
+  //   ++it_success;
+  //   ++it_mask;
+  // }
+  // if (args_info.debug_images_flag and !args_info.debug_only_flag)
+  //   syd::WriteImage<ImageType>(mask, "mask2.mhd");
 
-  // Redo with a mask
-  builder.ClearModel();
-  builder.debug_data.clear(); // need to reset the debug data
-  for(auto d:debug_points) builder.AddDebugPixel(d.name, d.x, d.y, d.z);
-  f3->id_ = 5; // to distinguish from previous
-  builder.AddModel(f3);
-  f4a->id_ = 6; // to distinguish from previous
-  builder.AddModel(f4a);
-  builder.restricted_tac_flag_ = true;
-  builder.CreateIntegratedActivityImage();
+  // // Redo with a mask
+  // builder.ClearModel();
+  // builder.debug_data.clear(); // need to reset the debug data
+  // for(auto d:debug_points) builder.AddDebugPixel(d.name, d.x, d.y, d.z);
+  // f3->id_ = 5; // to distinguish from previous
+  // builder.AddModel(f3);
+  // f4a->id_ = 6; // to distinguish from previous
+  // builder.AddModel(f4a);
+  // builder.restricted_tac_flag_ = true;
+  // builder.CreateIntegratedActivityImage();
 
-  // Output
-  if (args_info.debug_images_flag and !args_info.debug_only_flag)
-    for(auto o:builder.outputs_) syd::WriteImage<ImageType>(o->image, o->filename+"_2.mhd");
+  // // Output
+  // if (args_info.debug_images_flag and !args_info.debug_only_flag)
+  //   for(auto o:builder.outputs_) syd::WriteImage<ImageType>(o->image, o->filename+"_2.mhd");
 
-  // Debug here
-  builder.SaveDebugPixel("gp/tac_2.txt");
-  builder.SaveDebugModel("gp/models_2.txt");
+  // // Debug here
+  // builder.SaveDebugPixel("gp/tac_2.txt");
+  // builder.SaveDebugModel("gp/models_2.txt");
 
-  // update the mask
-  if (!args_info.debug_only_flag) {
-    it_success = success->iterator;
-    it_success.GoToBegin();
-    it_mask.GoToBegin();
-    while (!it_mask.IsAtEnd()) {
-      if (it_success.Get() == 1.0) it_mask.Set(0.0);
-      ++it_success;
-      ++it_mask;
-    }
-    if (args_info.debug_images_flag and !args_info.debug_only_flag)
-      syd::WriteImage<ImageType>(mask, "mask3.mhd");
+  // // update the mask
+  // if (!args_info.debug_only_flag) {
+  //   it_success = success->iterator;
+  //   it_success.GoToBegin();
+  //   it_mask.GoToBegin();
+  //   while (!it_mask.IsAtEnd()) {
+  //     if (it_success.Get() == 1.0) it_mask.Set(0.0);
+  //     ++it_success;
+  //     ++it_mask;
+  //   }
+  //   if (args_info.debug_images_flag and !args_info.debug_only_flag)
+  //     syd::WriteImage<ImageType>(mask, "mask3.mhd");
 
-    int f = syd::FillHoles<ImageType>(auc->image, mask, 2);
+  //   int f = syd::FillHoles<ImageType>(auc->image, mask, 2); FIXME
 
-    // Deal with remaining failed pixels
-    LOG(1) << "Last step: fill remaining holes. " << f << " failed pixels remain.";
+  //   // Deal with remaining failed pixels
+  //   LOG(1) << "Last step: fill remaining holes. " << f << " failed pixels remain.";
 
-    syd::WriteImage<ImageType>(auc->image, "auc3.mhd");
-  }
+  //   syd::WriteImage<ImageType>(auc->image, "auc3.mhd");
+  // }
 
   // Output
   DD("FIXME : insert builder output in the db");
