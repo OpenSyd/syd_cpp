f8bb42fd196fb78f896c06eff44509086d6a6706
David Sarrut <david.sarrut@creatis.insa-lyon.fr>
2016-01-22 15:51:01 +0100

    add output schema during odb compiler

--- cmake/UseSYD.cmake
+++ cmake/UseSYD.cmake
@@ -46,16 +46,18 @@ macro(WRAP_ODB ODB_SRCS)
     get_filename_component(ODB_BASEFILENAME ${ODB_FILES} NAME_WE)
     get_filename_component(ODB_FILES_ABS ${ODB_FILES} ABSOLUTE)
     set(ODB_CXX ${ODB_BASEFILENAME}-odb.cxx)
+    set(ODB_SCHEMA_CXX ${ODB_BASEFILENAME}-schema.cxx)
     set(ODB_IXX ${ODB_BASEFILENAME}-odb.ixx)
     set(ODB_HXX ${ODB_BASEFILENAME}-odb.hxx)
     set(ODB_OUTPUT
       ${CMAKE_CURRENT_BINARY_DIR}/${ODB_CXX}
+      ${CMAKE_CURRENT_BINARY_DIR}/${ODB_SCHEMA_CXX}
       ${CMAKE_CURRENT_BINARY_DIR}/${ODB_IXX}
       ${CMAKE_CURRENT_BINARY_DIR}/${ODB_HXX})
     if(EXISTS ${ODB_FILES_ABS})
       add_custom_command(OUTPUT ${ODB_OUTPUT}
         COMMAND ${ODB_EXECUTABLE}
-        ARGS --std c++11 --database sqlite -I${DCMTK_INCLUDE_DIR_BASE} -I${SYD_SOURCE_DIR}/src/std_db -I${SYD_SOURCE_DIR}/src/core  -I${SYD_SOURCE_DIR}/src/common_db  --generate-schema --schema-format embedded  --generate-query --sqlite-override-null --schema-name ${SCHEMA_NAME} ${ODB_FILES_ABS}
+        ARGS --std c++11 --database sqlite -I${DCMTK_INCLUDE_DIR_BASE} -I${SYD_SOURCE_DIR}/src/std_db -I${SYD_SOURCE_DIR}/src/core  -I${SYD_SOURCE_DIR}/src/common_db  --generate-schema --schema-format separate  --generate-query --sqlite-override-null --schema-name ${SCHEMA_NAME} ${ODB_FILES_ABS}
         DEPENDS ${ODB_FILES_ABS})
     else()
       message(FATAL_ERROR "Error odb cannot file the following file: " ${ODB_FILES_ABS})
