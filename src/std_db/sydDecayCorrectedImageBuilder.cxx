586e99f1474f0fe15d3587748d52960dd89b4c3c
David Sarrut <david.sarrut@creatis.insa-lyon.fr>
2016-02-01 14:16:11 +0100

    remove comments

--- src/std_db/sydDecayCorrectedImageBuilder.cxx
+++ src/std_db/sydDecayCorrectedImageBuilder.cxx
@@ -20,27 +20,6 @@
 #include "sydDecayCorrectedImageBuilder.h"
 
 // --------------------------------------------------------------------
-/*=========================================================================
-  Program:   syd
-
-  Authors belong to:
-  - University of LYON              http://www.universite-lyon.fr/
-  - Léon Bérard cancer center       http://www.centreleonberard.fr
-  - CREATIS CNRS laboratory         http://www.creatis.insa-lyon.fr
-
-  This software is distributed WITHOUT ANY WARRANTY; without even
-  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-  PURPOSE.  See the copyright notices for more information.
-
-  It is distributed under dual licence
-
-  - BSD        See included LICENSE.txt file
-  - CeCILL-B   http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html
-  ===========================================================================**/
-
-#include "sydDecayCorrectedImageBuilder.h"
-
-// --------------------------------------------------------------------
 syd::Image::pointer
 syd::DecayCorrectedImageBuilder::NewDecayCorrectedImage(syd::Image::pointer input)
 {
@@ -67,5 +46,4 @@ syd::DecayCorrectedImageBuilder::NewDecayCorrectedImage(syd::Image::pointer inpu
   SetImage<PixelType>(result, itk_image);
   return result;
 }
-// --------------------------------------------------------------------
 // --------------------------------------------------------------------
