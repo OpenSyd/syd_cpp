04eb79f37611b23d5d217b9e69e827ae8a9921bb
David Sarrut <david.sarrut@creatis.insa-lyon.fr>
2016-02-03 16:46:52 +0100

    correct calibration: divide by the injected activity

--- src/std_db/sydInsertCalibratedImage.cxx
+++ src/std_db/sydInsertCalibratedImage.cxx
@@ -62,8 +62,10 @@ int main(int argc, char* argv[])
   syd::ImageBuilder builder(db);
   for(auto image:images) {
     // Get the corresponding calibration
+    image->FatalIfNoDicom();
+    auto injection = image->dicoms[0]->injection;
     syd::Calibration::pointer calibration = db->FindCalibration(image, calib_tag);
-    double s = 1.0/calibration->factor;
+    double s = 1.0/calibration->factor / injection->activity_in_MBq;
     // Create an (empty) copy
     auto output = builder.NewMHDImageLike(image);
     db->SetImageTagsFromCommandLine(output, args_info);
