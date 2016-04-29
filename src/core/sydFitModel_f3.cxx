0d395edaeedf2df532e0f47ecfa884080caf1451
David Sarrut <david.sarrut@creatis.insa-lyon.fr>
2016-04-29 17:52:33 +0200

    add bounds on lambda

--- src/core/sydFitModel_f3.cxx
+++ src/core/sydFitModel_f3.cxx
@@ -52,9 +52,9 @@ void syd::FitModel_f3::SetProblemResidual(ceres::Problem * problem, syd::TimeAct
                               &params_[0], &params_[1], &params_[2]);
   }
 
+  problem->SetParameterLowerBound(&params_[1], 0, -0.9*GetLambdaPhysicHours());
   /*
   problem->SetParameterLowerBound(&params_[0], 0, 0.0);
-  problem->SetParameterLowerBound(&params_[1], 0, -0.9*GetLambdaPhysicHours());
   problem->SetParameterLowerBound(&params_[2], 0, 0.0);
   */
 }
