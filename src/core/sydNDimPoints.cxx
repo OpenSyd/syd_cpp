adb52002470ee09a86fa66e78468591c36ab258f
David Sarrut <david.sarrut@creatis.insa-lyon.fr>
2016-06-29 15:06:52 +0200

    add min max

--- src/core/sydNDimPoints.cxx
+++ src/core/sydNDimPoints.cxx
@@ -39,16 +39,42 @@ syd::NDimPoints::pointer syd::NDimPoints::New()
 void syd::NDimPoints::SetPointDimension(int d)
 {
   nb_dimensions = d;
-  for(auto v:values) delete v;
-  values.clear();
+  values.clear(); // when changing the dim, clear the data !
 }
 // --------------------------------------------------------------------
 
 
 // --------------------------------------------------------------------
-void syd::NDimPoints::push_back(double *v)
+double syd::NDimPoints::GetValue(int p, int d) const
 {
-  values.push_back(v);
+  return values[p*GetNumberOfDimensions() + d];
+}
+// --------------------------------------------------------------------
+
+
+// --------------------------------------------------------------------
+void syd::NDimPoints::SetValue(double v, int p, int d)
+{
+  values[p*GetNumberOfDimensions() + d] = v;
+}
+// --------------------------------------------------------------------
+
+
+// --------------------------------------------------------------------
+double * syd::NDimPoints::GetPointer()
+{
+  return &values[0];
+}
+// --------------------------------------------------------------------
+
+
+// --------------------------------------------------------------------
+//void syd::NDimPoints::push_back(double *v)
+double* syd::NDimPoints::push_back()
+{
+  for(auto i=0; i<GetNumberOfDimensions(); i++)
+    values.push_back(0.0);
+  return &(values[GetNumberOfPoints()*GetNumberOfDimensions()-GetNumberOfDimensions()]);
 }
 // --------------------------------------------------------------------
 
@@ -59,11 +85,11 @@ void syd::NDimPoints::Save(std::string filename)
   std::ofstream ofs(filename);//, std::ofstream::binary);
   //boost::archive::binary_oarchive oa(ofs);
   //oa << *this;
-  ofs << nb_dimensions << " "
-      << values.size() << std::endl;
+  ofs << GetNumberOfDimensions() << " "
+      << GetNumberOfPoints() << std::endl;
   for(auto & v:values)
-    for(auto i=0; i<nb_dimensions; i++)
-      ofs << v[i] << std::endl;
+    //    for(auto i=0; i<nb_dimensions; i++)
+    ofs << v << std::endl;
   ofs.close();
 }
 // --------------------------------------------------------------------
@@ -79,12 +105,34 @@ void syd::NDimPoints::Load(std::string filename)
   int n;
   ifs >> n;
   DD(n);
-  values.resize(n);
+  values.resize(n*nb_dimensions);
   for(auto & v:values) {
-    double * x = new double[nb_dimensions];
-    for(auto i=0; i<nb_dimensions; i++) ifs >> x[i];
-    v = x;
+    // double * x = new double[nb_dimensions];
+    // for(auto i=0; i<nb_dimensions; i++)
+    ifs >> v;//x[i];
+    //    v = x;
   }
   ifs.close();
+}
+// --------------------------------------------------------------------
+
+
+// --------------------------------------------------------------------
+void syd::NDimPoints::GetMinMax(std::vector<double> & mins,
+                                std::vector<double> & maxs)
+{
+  int N = GetNumberOfPoints();
+  int D = GetNumberOfDimensions();
+  mins.resize(D);
+  maxs.resize(D);
+  for(auto j=0; j<D; j++) {
+    mins[j] = std::numeric_limits<double>::max();
+    maxs[j] = std::numeric_limits<double>::lowest();
+    for(auto i=0; i<N; i++) {
+      double v = GetValue(i,j);
+      if (v < mins[j]) mins[j] = v;
+      if (v > maxs[j]) maxs[j] = v;
+    }
+  }
 }
 // --------------------------------------------------------------------
