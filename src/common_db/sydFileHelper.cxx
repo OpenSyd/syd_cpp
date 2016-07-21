f2b39bc89e8071dc0d7d7653f1951ec74be41fc5
David Sarrut <david.sarrut@creatis.insa-lyon.fr>
2016-07-21 16:14:28 +0200

    add file helper

---
+++ src/common_db/sydFileHelper.cxx
@@ -0,0 +1,47 @@
+/*=========================================================================
+  Program:   syd
+
+  Authors belong to:
+  - University of LYON              http://www.universite-lyon.fr/
+  - Léon Bérard cancer center       http://www.centreleonberard.fr
+  - CREATIS CNRS laboratory         http://www.creatis.insa-lyon.fr
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the copyright notices for more information.
+
+  It is distributed under dual licence
+
+  - BSD        See included LICENSE.txt file
+  - CeCILL-B   http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html
+  ===========================================================================**/
+
+// syd
+#include "sydFileHelper.h"
+#include "sydImageUtils.h"
+
+// --------------------------------------------------------------------
+void syd::FileHelper::RenameMHDFile(syd::File::pointer mhd_file,
+                                    syd::File::pointer raw_file,
+                                    std::string relative_path,
+                                    std::string filename)
+{
+  DD(mhd_file);
+  DD(raw_file);
+
+  // Rename mhd file
+  std::string old_path = mhd_file->GetAbsolutePath();
+  mhd_file->RenameFile(relative_path, filename, false); // do not move on disk
+  std::string new_path = mhd_file->GetAbsolutePath();
+
+  // Move file on disk
+  syd::RenameMHDImage(old_path, new_path);
+
+  // Rename raw file
+  std::string f = filename;
+  syd::Replace(f, ".mhd", ".raw");
+  raw_file->RenameFile(relative_path, f, false); // do not move on disk
+  DD(mhd_file);
+  DD(raw_file);
+}
+// --------------------------------------------------------------------
