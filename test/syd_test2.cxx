68e577bf051b860e8ad1dc4afe44178139ce5fa7
David Sarrut <david.sarrut@creatis.insa-lyon.fr>
2016-07-28 13:37:41 +0200

    add syd_test2

--- test/syd_test2.cxx
+++ test/syd_test2.cxx
@@ -19,6 +19,7 @@
 // syd
 #include "sydPluginManager.h"
 #include "sydDatabaseManager.h"
+#include "sydStandardDatabase.h"
 
 // --------------------------------------------------------------------
 int main(int argc, char* argv[])
@@ -31,21 +32,22 @@ int main(int argc, char* argv[])
   syd::DatabaseManager* m = syd::DatabaseManager::GetInstance();
 
   // Get the database
-  std::string dbname = "test1.db";
+  std::string dbname = "test2.db";
   std::string folder = "data";
 
   // Create StandardDatabase
   std::cout << "Create StandardDatabase " << dbname << std::endl;
   m->Create("StandardDatabase", dbname, folder, true);
 
-  std::cout << std::endl << "Open as generic Database" << std::endl;
-  syd::Database * db = m->Open(dbname);
+  std::cout << std::endl << "Open as StandardDatabase" << std::endl;
+  syd::StandardDatabase * db = m->Open<syd::StandardDatabase>(dbname);
   db->InsertDefaultRecords("all");
   db->Dump(std::cout);
 
   // Insert (plugin style)
   std::cout << std::endl << "Insert" << std::endl;
-  auto patient = db->New("Patient");
+  syd::Patient::pointer patient;
+  db->New(patient);
   std::vector<std::string> args = {"toto", "1", "50", "XXXXX", "M"};
   patient->Set(args);
   db->Insert(patient);
@@ -53,7 +55,8 @@ int main(int argc, char* argv[])
   if (patient->ToString() != "1 1 toto 50 XXXXX M")
     LOG(FATAL) << "Error insert patient";
 
-  auto injection = db->New("Injection");
+  syd::Injection::pointer injection;
+  db->New(injection);
   args = {"toto", "Lu-177", "2013-02-03 10:33", "188.3"};
   injection->Set(args);
   db->Insert(injection);
@@ -63,10 +66,12 @@ int main(int argc, char* argv[])
 
   // Update
   std::cout << std::endl << "Update" << std::endl;
-  db->Update(patient, "weight_in_kg", "100.0");
-  db->Update(injection, "activity_in_MBq", "666");
+  patient->weight_in_kg = 100.0;
+  injection->activity_in_MBq = 666;
   std::cout << "Patient  : " << patient << std::endl;
   std::cout << "Injection: " << injection << std::endl;
+  db->Update(patient);
+  db->Update(injection);
   if (patient->ToString() != "1 1 toto 100 XXXXX M")
     LOG(FATAL) << "Error update patient";
   if (injection->ToString() != "1 toto Lu-177 2013-02-03 10:33 666")
@@ -74,12 +79,24 @@ int main(int argc, char* argv[])
 
   // Query
   std::cout << std::endl << "Query" << std::endl;
-  syd::Record::pointer p2;
-  db->QueryOne(p2, "Patient", 1);
+  syd::Patient::pointer p2;
+  db->QueryOne(p2, 1);
   if (p2 != patient) LOG(FATAL) << "Error query patient";
-  syd::Record::pointer i2;
-  db->QueryOne(i2, "Injection", 1);
+  syd::Injection::pointer i2;
+  db->QueryOne(i2, 1);
   if (i2 != injection) LOG(FATAL) << "Error query injection";
+  std::cout << "Patient  : " << p2 << std::endl;
+  std::cout << "Injection: " << i2 << std::endl;
+
+  // Delete
+  std::cout << std::endl << "Delete" << std::endl;
+  db->Delete(injection); // first because foreign_keys for patient
+  db->Delete(patient);
+  int n = db->GetNumberOfElements("Patient");
+  if (n != 0) LOG(FATAL) << "Error delete patient";
+  n = db->GetNumberOfElements("Injection");
+  if (n != 0) LOG(FATAL) << "Error delete injection";
+  std::cout << n << std::endl;
 
 
   return EXIT_SUCCESS;
