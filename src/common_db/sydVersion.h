0afb5fe92358ad362a446fc3768a0e90b6c07408
David Sarrut <david.sarrut@creatis.insa-lyon.fr>
2016-10-12 18:02:57 +0200

    close the db structure

--- src/common_db/sydVersion.h
+++ src/common_db/sydVersion.h
@@ -60,7 +60,7 @@
 #define BUILD_VERSION(x,y)  PASTER(x,y)
 
 #ifdef ODB_COMPILER
-#pragma db model version(BUILD_VERSION(,SYD_BASE_VERSION), BUILD_VERSION(,SYD_VERSION), open)
+#pragma db model version(BUILD_VERSION(,SYD_BASE_VERSION), BUILD_VERSION(,SYD_VERSION), closed)
 #endif
 
 #endif
