13304d3786e223ecae95c5473df7b922313f7f93
David Sarrut <david.sarrut@creatis.insa-lyon.fr>
2016-10-24 15:46:25 +0200

    remove debug verbose

--- src/core/sydTimeIntegratedActivityImageFilter.cxx
+++ src/core/sydTimeIntegratedActivityImageFilter.cxx
@@ -28,7 +28,6 @@
 syd::TimeIntegratedActivityImageFilter::TimeIntegratedActivityImageFilter():
   TimeIntegratedActivityFilter()
 {
-  DDF();
   mask_ = nullptr;
 }
 // --------------------------------------------------------------------
