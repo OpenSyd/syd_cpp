b2b59ab73e9f1108047ea69b1f3d8cc5cbdf405f
David Sarrut <david.sarrut@creatis.insa-lyon.fr>
2016-11-06 17:45:55 +0100

    change value to float to allow direct gui

--- src/core/sydTimeIntegratedActivityFitOptions.h
+++ src/core/sydTimeIntegratedActivityFitOptions.h
@@ -36,7 +36,7 @@ namespace syd {
     ~TimeIntegratedActivityFitOptions();
 
     // Input
-    void SetR2MinThreshold(double r) { R2_min_threshold_ = r; }
+    void SetR2MinThreshold(float r) { R2_min_threshold_ = r; }
     void SetRestrictedFlag(bool b) { restricted_flag_ = b; }
     void AddModel(const std::string & model_name);
     void ClearModels() { model_names_.clear(); }
@@ -48,9 +48,12 @@ namespace syd {
     // Get model object
     syd::FitModelBase::vector GetModels() const;
     std::string GetModelsName() const;
-    double GetR2MinThreshold() const { return R2_min_threshold_; }
+    float GetR2MinThreshold() const { return R2_min_threshold_; }
+    float & GetR2MinThreshold() { return R2_min_threshold_; }
     bool GetRestrictedFlag() const { return restricted_flag_; }
+    bool & GetRestrictedFlag() { return restricted_flag_; }
     int GetMaxNumIterations() const { return max_num_iterations_; }
+    int & GetMaxNumIterations() { return max_num_iterations_; }
     std::string GetAkaikeCriterion() const { return akaike_criterion_; }
     double GetLambdaDecayConstantInHours() const { return lambda_in_hours_; }
     std::string ToString() const;
@@ -64,7 +67,7 @@ namespace syd {
 
   protected:
     bool restricted_flag_;
-    double R2_min_threshold_;
+    float R2_min_threshold_;
     std::set<std::string> model_names_;
     syd::FitModelBase::vector all_models_;
     int max_num_iterations_;
