8225207624919cff258110b44d49b2cbdb8070d1
David Sarrut <david.sarrut@creatis.insa-lyon.fr>
2016-11-11 17:35:37 +0100

    default is now 50 not 100

--- src/core/sydTimeIntegratedActivityFitOptions.cxx
+++ src/core/sydTimeIntegratedActivityFitOptions.cxx
@@ -26,7 +26,7 @@ syd::TimeIntegratedActivityFitOptions::TimeIntegratedActivityFitOptions()
 {
   SetR2MinThreshold(0.9);
   SetRestrictedFlag(false);
-  SetMaxNumIterations(100);
+  SetMaxNumIterations(50);
   SetAkaikeCriterion("AIC");
   SetLambdaDecayConstantInHours(0.0);
   auto f1  = std::make_shared<syd::FitModel_f1>();
