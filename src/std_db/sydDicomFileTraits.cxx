a1fe6ac463ba19a835324ee3a80181708644cbe0
David Sarrut <david.sarrut@creatis.insa-lyon.fr>
2017-02-15 17:51:22 +0100

    traits for dicomserie dicomfile

--- src/std_db/sydDicomFileTraits.cxx
+++ src/std_db/sydDicomFileTraits.cxx
@@ -26,43 +26,24 @@ DEFINE_TABLE_TRAITS_IMPL(DicomFile);
 
 
 // --------------------------------------------------------------------
-template<> void syd::RecordTraits<syd::DicomFile>::
-BuildMapOfSortFunctions(CompareFunctionMap & map) const
+template<>
+void
+syd::RecordTraits<syd::DicomFile>::
+BuildFields(const syd::Database * db) const
 {
-  // Sort functions from File
-  syd::RecordTraits<syd::File>::CompareFunctionMap m;
-  syd::RecordTraits<syd::File>::GetTraits()->BuildMapOfSortFunctions(m);
-  map.insert(m.begin(), m.end());
+  InitCommonFields();
 
-  // New sort comparison
-  auto f = [](pointer a, pointer b) -> bool { return a->dicom_sop_uid < b->dicom_sop_uid; };
-  map["dicom_sop_uid"] = f;
-  map[""] = f; // make this one the default
-  map["dicom_instance_number"] = [](pointer a, pointer b) ->
-    bool { return a->dicom_instance_number < b->dicom_instance_number; };
+  ADD_FIELD(dicom_sop_uid, std::string);
+  ADD_FIELD(dicom_instance_number, int);
+
+  // Inherit File
+  field_map_["filename"] = db->NewField("File", "filename");
+  field_map_["path"] = db->NewField("File", "path");
+  field_map_["md5"] = db->NewField("File", "md5");
+
+  // Format lists
+  field_format_map_["default"] =
+    "id filename path dicom_instance_number dicom_sop_uid md5";
 }
 // --------------------------------------------------------------------
 
-
-// --------------------------------------------------------------------
-template<> void syd::RecordTraits<syd::DicomFile>::
-BuildMapOfFieldsFunctions(FieldFunctionMap & map) const
-{
-  // Inherit from File
-  syd::RecordTraits<syd::File>::FieldFunctionMap m;
-  syd::RecordTraits<syd::File>::GetTraits()->BuildMapOfFieldsFunctions(m);
-  map.insert(m.begin(), m.end());
-  map["dicom_sop_uid"] = [](pointer a) -> std::string { return a->dicom_sop_uid; };
-  map["dicom_instance_number"] = [](pointer a) -> std::string { return syd::ToString(a->dicom_instance_number,0); };
-}
-// --------------------------------------------------------------------
-
-
-// --------------------------------------------------------------------
-template<> std::string syd::RecordTraits<syd::DicomFile>::
-GetDefaultFields() const
-{
-  std::string s = "id filename dicom_instance_number dicom_sop_uid path";
-  return s;
-}
-// --------------------------------------------------------------------
