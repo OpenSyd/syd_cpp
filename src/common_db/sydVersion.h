2d881eb91c40af8451b9a0c745d28ed4d3da0925
David Sarrut <david.sarrut@gmail.com>
2017-05-13 16:54:11 +0200

    close version

--- src/common_db/sydVersion.h
+++ src/common_db/sydVersion.h
@@ -26,10 +26,11 @@
 // the change is legitimate: 1) open and 2) increment the
 // version. 3) Once checked, close again.
 // It  means that we can migrate from base version to current version)
-#define SYD_VERSION      0126 // version 1.26 (current version)
+#define SYD_VERSION      0127 // version 1.27 (current version)
 #define SYD_BASE_VERSION 0101 // version 1.1 (base version)
 
 // Log:
+// 1.27 : patient with multiple dicom_patient_id
 // 1.26 : add injection to DicomSerie
 // 1.25 : add other dicom_tags to DicomSerie
 // 1.24 : add dicom_tags to DicomSerie
@@ -67,7 +68,7 @@
 #define BUILD_VERSION(x,y)  PASTER(x,y)
 
 #ifdef ODB_COMPILER
-#pragma db model version(BUILD_VERSION(,SYD_BASE_VERSION), BUILD_VERSION(,SYD_VERSION), closed)
+//#pragma db model version(BUILD_VERSION(,SYD_BASE_VERSION), BUILD_VERSION(,SYD_VERSION), closed)
 //#pragma db model version(BUILD_VERSION(,SYD_BASE_VERSION), BUILD_VERSION(,SYD_VERSION), open)
 #endif
 
