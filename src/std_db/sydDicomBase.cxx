3c6664f91cb2ab811dfc82c43455cb34255cb8b7
David Sarrut <david.sarrut@creatis.insa-lyon.fr>
2017-05-15 17:07:29 +0200

    remove comments

--- src/std_db/sydDicomBase.cxx
+++ src/std_db/sydDicomBase.cxx
@@ -98,23 +98,6 @@ void syd::DicomBase::Callback(odb::callback_event event, odb::database & db)
 
 
 // --------------------------------------------------------------------
-// std::string syd::DicomBase::ComputeRelativeFolder() const
-// {
-//   if (patient == NULL) {
-//     LOG(FATAL) << "Error calling ComputeRelativeFolder for a serie, patient pointer is not set. "
-//                << ToString();
-//   }
-//   // Part 1: patient
-//   std::string f = patient->ComputeRelativeFolder()+PATH_SEPARATOR;
-
-//   // Part 3: modality
-//   f = f+PATH_SEPARATOR+dicom_modality;
-//   return f;
-// }
-// --------------------------------------------------------------------
-
-
-// --------------------------------------------------------------------
 syd::CheckResult syd::DicomBase::Check() const
 {
   syd::CheckResult r;
